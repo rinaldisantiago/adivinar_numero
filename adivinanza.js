12a541f4856985e67c3bdbda06c75a7cf045587b
rinaldisantiago <sanrinaldi99@gmail.com>
2023-12-12 22:44:14 -0300

    first commit

---
+++ adivinanza.js
@@ -0,0 +1,15 @@
+const generarNumeroAleatorio = () => {
+    return Math.floor(Math.random() * 100) + 1;
+  };
+  
+  const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+    if (numeroAdivinado == numeroSecreto) {
+      console.log("¡Felicidades, has adivinado el numero secreto!");
+    } else if (numeroAdivinado > numeroSecreto) {
+      console.log("El numero secreto es menor. ¡Sigue intentando!.");
+    } else {
+      console.log("El numero secreto es mayor. ¡Sigue intentando!");
+    }
+  };
+  
+  module.exports = { generarNumeroAleatorio, verificarAdivinanza };
